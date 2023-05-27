a0ce55f97462c21a82081a84630b68d22841061d
Alexandr Nikishin <Alexanclr.Nikishin@yandex.ru>
2023-05-27 14:21:33 +0300

    xxx

---
+++ scripts/Card.js
@@ -0,0 +1,54 @@
+class Card {
+  constructor (data, templateSelector, openFigurePopup) {
+    this._name = data.name;
+    this._link = data.link;
+    this._templateSelector = templateSelector;
+    this._element = this._getTemplate();
+    this.cardPhoto = this._element.querySelector(".card__photo");
+    this.cardTitle = this._element.querySelector(".card__title");
+    this.cardLikeButton = this._element.querySelector(".card__like-button");
+    this.cardDeleteButton = this._element.querySelector(".card__delete-button");
+    this.openFigurePopup = openFigurePopup;
+  };
+
+  //получаем темплейт карточки
+  _getTemplate() {
+    const cardElement = this._templateSelector.content.querySelector('.card').cloneNode(true);
+    return cardElement;
+  };
+
+  //метод подстановки данных в темплейт и установки обработчиков событий
+  generateCard() {
+    this.cardTitle.textContent = this._name;
+    this.cardPhoto.src = this._link;
+    this.cardPhoto.alt = this._name;
+    this._setEventListeners();
+    return this._element;
+  };
+
+  //метод активации лайка
+  _handleLike() {
+    this.cardLikeButton.classList.toggle('card__like-button_active');
+  };
+
+   //метод удаления карточки
+  _handleDelete() {
+    this._element.remove();
+  };
+
+  _setEventListeners = () => {
+
+    this.cardDeleteButton.addEventListener('click', () => {
+      this._handleDelete();
+    });
+
+    this.cardLikeButton.addEventListener('click', () => {
+      this._handleLike();
+    });
+
+    this.cardPhoto.addEventListener('click', () => {
+      this.openFigurePopup(this._name, this._link);
+    });
+  };
+};
+export { Card };
---
+++ scripts/FormValidator.js
@@ -0,0 +1,68 @@
+class FormValidator { //принимает  конфиг и форму
+  constructor (config, form) {
+    this.config = config;
+    this.form = form;
+    this.inputs = Array.from(this.form.querySelectorAll(config.inputSelector));//инпуты в форме
+    this.popupSubmitButton = this.form.querySelector(config.submitButtonSelector);//кнопка
+  };
+
+  //метод показывает сообщение об ошибке
+_showInputError(input) {
+  const errorElement = this.form.querySelector(`#error-${input.id}`)
+  input.classList.add(this.config.inputErrorClass);
+  errorElement.textContent = input.validationMessage;
+};
+
+//метод убирает сообщение об ошибке
+_hideInputError(input) {
+  const errorElement = this.form.querySelector(`#error-${input.id}`)
+  input.classList.remove(this.config.inputErrorClass)
+  errorElement.textContent = '';
+};
+
+//метод проверяет инпут на валидность
+_checkInputValidity(input) {
+  if (input.validity.valid) {
+    this._hideInputError(input);
+  } else {
+    this._showInputError(input);
+  }
+};
+
+  //неактивная кнопка сабмита
+_setButtonDisable() {
+  this.popupSubmitButton.setAttribute('disabled', '');
+  this.popupSubmitButton.classList.add(this.config.inactiveButtonClass);
+};
+
+//активная кнопка сабмита
+_setButtonEnable() {
+  this.popupSubmitButton.removeAttribute('disabled');
+  this.popupSubmitButton.classList.remove(this.config.inactiveButtonClass);
+};
+
+//функция переключающая состояния сабмита
+_toggleButtonValidity() {
+if (this.form.checkValidity()) {
+  this._setButtonEnable()
+} else {
+  this._setButtonDisable();
+}
+};
+    _setEventListeners = () => {
+      this._toggleButtonValidity();
+      this.inputs.forEach((input) => {
+        input.addEventListener('input', () => {
+          this._checkInputValidity(input);
+          this._toggleButtonValidity();
+        });
+      });
+    };
+
+
+  //метод enableValidation, чтобы включить валидацию
+  enableValidation = () => {
+    this._setEventListeners();
+  };
+};
+export { FormValidator };
